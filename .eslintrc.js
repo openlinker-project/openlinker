0f2da8f5a317a2135c4cdd5ec1d8d99e82931d4b
Piotr Swierzy <piotr.swierzy@blockydevs.com>
2025-12-20 15:22:55 +0100

    fix: lint config path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['./tsconfig.eslint.json'],
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
@@ -36,5 +36,13 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['**/*.spec.ts', '**/*.test.ts'],
+      rules: {
+        '@typescript-eslint/unbound-method': 'off',
+      },
+    },
+  ],
 };
 
--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,10 +2,5 @@ const baseConfig = require('../../.eslintrc.js');
 
 module.exports = {
   ...baseConfig,
-  parserOptions: {
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
   root: false,
 };
